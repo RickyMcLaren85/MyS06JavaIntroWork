6dcc94539bfc46e8372ae81f9e72a7c5f157bef1
RickyMcLaren <rickymclaren@hotmail.com>
2023-04-17 20:29:35 +0100

    Work up to now.

--- src/SumAvgArray.java
+++ src/SumAvgArray.java
@@ -1,2 +1,12 @@
 public class SumAvgArray {
+    public static void main(String[] args) {
+        int sum = 0;
+        int [] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+        }
+        System.out.println("Sum: " + sum);
+        double average = (double) sum / numbers.length;
+        System.out.println("Average: " + average);
+    }
 }

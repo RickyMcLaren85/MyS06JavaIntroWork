98dffc35a2eed20ccbc419d42c6654efc605c354
RickyMcLaren <rickymclaren@hotmail.com>
2023-04-17 21:29:23 +0100

    Work up to now.

---
+++ src/ReverseString.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class ReverseString {
+    public static void main(String[] args) {
+        System.out.println("--This program will reverse any word!--");
+        Scanner reader = new Scanner (System.in);
+        char[] letters= reader.nextLine().toCharArray();
+        for (int i = letters.length - 1; i >= 0; i--) {
+            System.out.print(letters[i]);
+        }
+    }
+}
